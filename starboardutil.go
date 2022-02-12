f737fe73e15c84e5a87524b4867b42d1a9c82480
l1ving <17222512+l1ving@users.noreply.github.com>
2022-02-11 21:16:58 -0500

    fix: Context-ify `starboardutil.go`

--- starboardutil.go
+++ starboardutil.go
@@ -6,12 +6,10 @@ import (
 	"log"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
 type StarboardConfig struct {
-	Mutex       sync.Mutex         `json:"-"`                      // not saved in DB
 	Channel     int64              `json:"channel,omitempty"`      // channel post ID
 	NsfwChannel int64              `json:"nsfw_channel,omitempty"` // nsfw post channel ID
 	Messages    []StarboardMessage `json:"messages,omitempty"`
@@ -41,44 +39,37 @@ func StarboardReactionHandler(e *gateway.MessageReactionAddEvent) {
 			g.Starboard.Threshold = 3
 		}
 
-		// Prevent inadvertent modifying of the starboard while this runs
-		g.Starboard.Mutex.Lock()
-
-		return g, "StarboardReactionHandler: update threshold"
-	})
-
-	// Not starred by a guild member
-	if e.Member == nil {
-		log.Printf("Not a guild member\n")
-		return
-	}
-
-	// Not a star
-	if e.Emoji.APIString().PathString() != escapedStar {
-		log.Printf("Not a star emoji\n")
-		return
-	}
-
-	msg, err := discordClient.Message(e.ChannelID, e.MessageID)
-	if err != nil {
-		if *debug {
-			log.Printf("Error retrieving starred message: %v\n", err)
+		// Not starred by a guild member
+		if e.Member == nil {
+			log.Printf("Not a guild member\n")
+			return g, "StarboardReactionHandler: check guild member"
 		}
-		return
-	}
-	channel, err := discordClient.Channel(e.ChannelID)
-	if err != nil {
-		if *debug {
-			log.Printf("Error retrieving starred message channel: %v\n", err)
+
+		// Not a star
+		if e.Emoji.APIString().PathString() != escapedStar {
+			log.Printf("Not a star emoji\n")
+			return g, "StarboardReactionHandler: check reaction emoji"
 		}
-		return
-	}
 
-	var sMsg *StarboardMessage = nil
-	newPost := true
-	cID := int64(channel.ID)
+		msg, err := discordClient.Message(e.ChannelID, e.MessageID)
+		if err != nil {
+			if *debug {
+				log.Printf("Error retrieving starred message: %v\n", err)
+			}
+			return g, "StarboardReactionHandler: get reaction message"
+		}
+		channel, err := discordClient.Channel(e.ChannelID)
+		if err != nil {
+			if *debug {
+				log.Printf("Error retrieving starred message channel: %v\n", err)
+			}
+			return g, "StarboardReactionHandler: get reaction channel"
+		}
 
-	GuildContext(e.GuildID, func(g *GuildConfig) (*GuildConfig, string) {
+		var sMsg *StarboardMessage = nil
+		newPost := true
+		cID := int64(channel.ID)
+
 		// If user reacts to a post in a starboard channel
 		if cID == g.Starboard.Channel || cID == g.Starboard.NsfwChannel {
 			for _, m := range g.Starboard.Messages {
@@ -107,128 +98,120 @@ func StarboardReactionHandler(e *gateway.MessageReactionAddEvent) {
 		if sMsg.IsNsfw == true {
 			cID = g.Starboard.NsfwChannel
 		}
-		return g, "StarboardReactionHandler: generate starboard post"
-	})
 
-	// Channel hasn't been set
-	if cID == 0 {
-		log.Printf("Channel ID is 0\n")
-		return
-	}
-
-	// Get post channel and ensure it exists
-	postChannel, err := discordClient.Channel(discord.ChannelID(cID))
-	if err != nil {
-		log.Printf("Couldn't get post channel\n")
-		return
-	}
-
-	// When adding a new star, ensure star user is not the same as author
-	// And also check if they've already been added
-	sUserID := int64(e.Member.User.ID)
-	if sMsg.Author != sUserID && !Int64SliceContains(sMsg.Stars, sUserID) {
-		sMsg.Stars = append(sMsg.Stars, sUserID)
-	}
-	log.Printf("sUserID: %v\nsMsg:%v\n", sUserID, sMsg)
-
-	// Update our reactions in case any are missing from the API
-	for _, reaction := range msg.Reactions {
-		if reaction.Emoji.APIString().PathString() == escapedStar {
-			userReactions, err := discordClient.Reactions(msg.ChannelID, msg.ID, reaction.Emoji.APIString(), 0)
-			if err != nil {
-				log.Printf("Failed to get userReactions: %s\n", err)
-				return
-			}
-
-			for _, userReaction := range userReactions {
-				sUserID = int64(userReaction.ID)
-
-				if sMsg.Author != sUserID && !Int64SliceContains(sMsg.Stars, sUserID) {
-					sMsg.Stars = append(sMsg.Stars, sUserID)
-				}
-			}
-			break
+		// Channel hasn't been set
+		if cID == 0 {
+			log.Printf("Channel ID is 0\n")
+			return g, "StarboardReactionHandler: check cID"
 		}
-	}
 
-	stars := len(sMsg.Stars)
-	notEnoughStars := false
-	GuildContext(e.GuildID, func(g *GuildConfig) (*GuildConfig, string) {
-		notEnoughStars = int64(stars) < g.Starboard.Threshold
-		return g, "StarboardReactionHandler: check notEnoughStars"
-	})
+		// Get post channel and ensure it exists
+		postChannel, err := discordClient.Channel(discord.ChannelID(cID))
+		if err != nil {
+			log.Printf("Couldn't get post channel\n")
+			return g, "StarboardReactionHandler: get post channel"
+		}
 
-	// Not enough stars in sMsg to make post
-	if notEnoughStars {
-		log.Printf("Not enough stars: %v\n", sMsg.Stars)
-		return
-	}
+		// When adding a new star, ensure star user is not the same as author
+		// And also check if they've already been added
+		sUserID := int64(e.Member.User.ID)
+		if sMsg.Author != sUserID && !Int64SliceContains(sMsg.Stars, sUserID) {
+			sMsg.Stars = append(sMsg.Stars, sUserID)
+		}
+		log.Printf("sUserID: %v\nsMsg:%v\n", sUserID, sMsg)
 
-	content := getEmoji(stars) + " **" + strconv.Itoa(stars) + "** <#" + strconv.FormatInt(int64(msg.ChannelID), 10) + ">"
+		// Update our reactions in case any are missing from the API
+		for _, reaction := range msg.Reactions {
+			if reaction.Emoji.APIString().PathString() == escapedStar {
+				userReactions, err := discordClient.Reactions(msg.ChannelID, msg.ID, reaction.Emoji.APIString(), 0)
+				if err != nil {
+					log.Printf("Failed to get userReactions: %s\n", err)
+					return g, "StarboardReactionHandler: update sMsg.Stars"
+				}
 
-	// Attempt to get existing message, and make a new one if it isn't there
-	pMsg, err := discordClient.Message(postChannel.ID, discord.MessageID(sMsg.PostID))
-	if err != nil {
-		log.Printf("Couldn't get pMsg %v\n", err)
+				for _, userReaction := range userReactions {
+					sUserID = int64(userReaction.ID)
 
-		// Construct new starboard post if it couldn't retrieve an existing one
-
-		// Try to find a URL in the message content
-		description := msg.Content
-		url := urlRegex.MatchString(msg.Content)
-
-		// Set the embed image to the URL and try to find the first attached image in the message attachments
-		var image *discord.EmbedImage = nil
-		for _, attachment := range msg.Attachments {
-			if strings.HasPrefix(attachment.ContentType, "image/") {
-				image = &discord.EmbedImage{URL: attachment.URL}
-				url = false // Don't remove URL in embed if we found an image attachment (eg, twitter link + image attachment)
+					if sMsg.Author != sUserID && !Int64SliceContains(sMsg.Stars, sUserID) {
+						sMsg.Stars = append(sMsg.Stars, sUserID)
+					}
+				}
 				break
 			}
 		}
 
-		// If we found only a URL (no other text) in the message content, and the found URL has an image extension, and we didn't find an attached image
-		// Set the description to nothing and set the image to the found URL
-		if url && FileExtMatches(imageExtensions, msg.Content) {
-			description = ""
-			image = &discord.EmbedImage{URL: msg.Content}
+		stars := len(sMsg.Stars)
+
+		// Not enough stars in sMsg to make post
+		if int64(stars) < g.Starboard.Threshold {
+			log.Printf("Not enough stars: %v\n", sMsg.Stars)
+			return g, "StarboardReactionHandler: check notEnoughStars"
 		}
 
-		member, err := discordClient.Member(e.GuildID, discord.UserID(sMsg.Author))
+		content := getEmoji(stars) + " **" + strconv.Itoa(stars) + "** <#" + strconv.FormatInt(int64(msg.ChannelID), 10) + ">"
+
+		// Attempt to get existing message, and make a new one if it isn't there
+		pMsg, err := discordClient.Message(postChannel.ID, discord.MessageID(sMsg.PostID))
 		if err != nil {
-			log.Printf("Couldn't get member %v\n", err)
-			return
-		}
+			log.Printf("Couldn't get pMsg %v\n", err)
 
-		field := discord.EmbedField{Name: "Source", Value: CreateMessageLink(int64(e.GuildID), msg, true)}
-		footer := discord.EmbedFooter{Text: strconv.FormatInt(sMsg.Author, 10)}
-		embed := discord.Embed{
-			Description: description,
-			Author:      CreateEmbedAuthor(*member),
-			Fields:      []discord.EmbedField{field},
-			Footer:      &footer,
-			Timestamp:   msg.Timestamp,
-			Color:       starboardColor,
-			Image:       image,
-		}
+			// Construct new starboard post if it couldn't retrieve an existing one
 
-		log.Printf("Embed image: %v\n", embed.Image)
+			// Try to find a URL in the message content
+			description := msg.Content
+			url := urlRegex.MatchString(msg.Content)
 
-		msg, err = discordClient.SendMessage(postChannel.ID, content, embed)
-		if err != nil {
-			log.Printf("Error sending starboard post: %v\n", err)
+			// Set the embed image to the URL and try to find the first attached image in the message attachments
+			var image *discord.EmbedImage = nil
+			for _, attachment := range msg.Attachments {
+				if strings.HasPrefix(attachment.ContentType, "image/") {
+					image = &discord.EmbedImage{URL: attachment.URL}
+					url = false // Don't remove URL in embed if we found an image attachment (eg, twitter link + image attachment)
+					break
+				}
+			}
+
+			// If we found only a URL (no other text) in the message content, and the found URL has an image extension, and we didn't find an attached image
+			// Set the description to nothing and set the image to the found URL
+			if url && FileExtMatches(imageExtensions, msg.Content) {
+				description = ""
+				image = &discord.EmbedImage{URL: msg.Content}
+			}
+
+			member, err := discordClient.Member(e.GuildID, discord.UserID(sMsg.Author))
+			if err != nil {
+				log.Printf("Couldn't get member %v\n", err)
+				return g, "StarboardReactionHandler: get sMsg.Author"
+			}
+
+			field := discord.EmbedField{Name: "Source", Value: CreateMessageLink(int64(e.GuildID), msg, true)}
+			footer := discord.EmbedFooter{Text: strconv.FormatInt(sMsg.Author, 10)}
+			embed := discord.Embed{
+				Description: description,
+				Author:      CreateEmbedAuthor(*member),
+				Fields:      []discord.EmbedField{field},
+				Footer:      &footer,
+				Timestamp:   msg.Timestamp,
+				Color:       starboardColor,
+				Image:       image,
+			}
+
+			log.Printf("Embed image: %v\n", embed.Image)
+
+			msg, err = discordClient.SendMessage(postChannel.ID, content, embed)
+			if err != nil {
+				log.Printf("Error sending starboard post: %v\n", err)
+			} else {
+				sMsg.PostID = int64(msg.ID)
+			}
 		} else {
-			sMsg.PostID = int64(msg.ID)
+			// Edit the post if it exists
+			_, err = discordClient.EditMessage(postChannel.ID, discord.MessageID(sMsg.PostID), content, pMsg.Embeds...)
+			if err != nil {
+				log.Printf("Error updating starboard post: %v\n", err)
+			}
 		}
-	} else {
-		// Edit the post if it exists
-		_, err = discordClient.EditMessage(postChannel.ID, discord.MessageID(sMsg.PostID), content, pMsg.Embeds...)
-		if err != nil {
-			log.Printf("Error updating starboard post: %v\n", err)
-		}
-	}
 
-	GuildContext(e.GuildID, func(g *GuildConfig) (*GuildConfig, string) {
 		// Now that we have updated the stars and starboard post ID, save it in the config
 		if newPost {
 			g.Starboard.Messages = append(g.Starboard.Messages, *sMsg)
@@ -239,8 +222,6 @@ func StarboardReactionHandler(e *gateway.MessageReactionAddEvent) {
 				}
 			}
 		}
-		// We are done with the starboard, unlock it
-		g.Starboard.Mutex.Unlock()
 
 		return g, "StarboardReactionHandler: update post"
 	})
