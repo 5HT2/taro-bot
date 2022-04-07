cc9d042dbf873ef748f54aecc887929f94d4289a
5HT2 <git@frogg.ie>
2022-04-07 08:34:59 -0400

    cleanup: Remove unused `CopyFile`

--- util.go
+++ util.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"image/color"
-	"io"
-	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -12,28 +10,6 @@ import (
 var (
 	imageExtensions = []string{".jpg", ".jpeg", ".png", ".gif", ".gifv"}
 )
-
-// CopyFile the src file to dst. Any existing file will be overwritten and will not
-// copy file attributes.
-func CopyFile(src, dst string) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
-	}
-	return out.Close()
-}
 
 func FileExtMatches(s []string, file string) bool {
 	found := false
