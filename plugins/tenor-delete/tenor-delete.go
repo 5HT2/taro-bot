5365860d483f1fcb3718927a75e0db083a3fa6d6
5HT2 <git@frogg.ie>
2022-05-11 04:43:05 -0400

    plugins: Add `tenor-delete`

---
+++ plugins/tenor-delete/tenor-delete.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"github.com/5HT2/taro-bot/bot"
+	"github.com/5HT2/taro-bot/cmd"
+	"github.com/5HT2/taro-bot/plugins"
+	"github.com/5HT2/taro-bot/util"
+	"log"
+	"regexp"
+)
+
+var (
+	tenorRegex = regexp.MustCompile(`http(s)?://t([ex])nor\.[A-z]+/view/.*`)
+)
+
+func InitPlugin(_ *plugins.PluginInit) *plugins.Plugin {
+	return &plugins.Plugin{
+		Name:        "Tenor Delete",
+		Description: "Automatically delete tenor gifs",
+		Version:     "1.0.0",
+		Commands: []bot.CommandInfo{{
+			Fn:          TenorDeleteCommand,
+			FnName:      "TenorDeleteCommand",
+			Name:        "tenordelete",
+			Description: "Toggle tenor deletion on or off"},
+		},
+		Responses: []bot.ResponseInfo{{
+			Fn:       TenorDeleteResponse,
+			Silent:   true,
+			Regexes:  []string{tenorRegex.String()},
+			MatchMin: 1,
+		}},
+	}
+}
+
+func TenorDeleteResponse(r bot.Response) string {
+	bot.GuildContext(r.E.GuildID, func(g *bot.GuildConfig) (*bot.GuildConfig, string) {
+		if util.SliceContains(g.EnabledTenorDelete, int64(r.E.GuildID)) {
+			if err := bot.Client.DeleteMessage(r.E.ChannelID, r.E.Message.ID, "Matched Tenor gif"); err != nil {
+				log.Printf("TenorDeleteResponse: %v\n", err)
+			}
+		}
+
+		return g, "TenorDeleteResponse"
+	})
+
+	return ""
+}
+
+func TenorDeleteCommand(c bot.Command) error {
+	var err error
+
+	bot.GuildContext(c.E.GuildID, func(g *bot.GuildConfig) (*bot.GuildConfig, string) {
+		id := int64(c.E.GuildID)
+
+		if util.SliceContains(g.EnabledTenorDelete, id) {
+			g.EnabledTenorDelete = util.SliceRemove(g.EnabledTenorDelete, id)
+			_, err = cmd.SendEmbed(c, "Tenor Delete", "⛔ Disabled Tenor Delete for this guild", bot.ErrorColor)
+		} else {
+			g.EnabledTenorDelete = append(g.EnabledTenorDelete, id)
+			_, err = cmd.SendEmbed(c, "Tenor Delete", "✅ Enabled Tenor Delete for this guild", bot.SuccessColor)
+		}
+
+		return g, "TenorDeleteCommand"
+	})
+
+	return err
+}
