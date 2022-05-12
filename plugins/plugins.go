b3692fdfc76d87497e2610e9e64c52c4f1f31e06
5HT2 <git@frogg.ie>
2022-05-12 03:40:01 -0400

    refactor: Move `meta.go` to `plugins.go`

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -27,12 +27,14 @@ func (p Plugin) String() string {
 	return fmt.Sprintf("[%s, %s, %v, %s, %s, %s]", p.Name, p.Description, p.Version, p.Commands, p.Responses, p.Jobs)
 }
 
+// Register will register a plugin's commands, responses and jobs to the bot
 func (p *Plugin) Register() {
 	bot.Commands = append(bot.Commands, p.Commands...)
 	bot.Responses = append(bot.Responses, p.Responses...)
 	bot.Jobs = append(bot.Jobs, p.Jobs...) // these need to have RegisterJobs called in order to function
 }
 
+// Load will load all the plugins from dir specified in pluginList
 func Load(dir, pluginList string) {
 	d, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -71,6 +73,50 @@ func Load(dir, pluginList string) {
 			}
 		}
 	}
+}
+
+// ClearJobs will go through bot.Jobs and handle the de-registration of them
+func ClearJobs() {
+	for _, job := range bot.Jobs {
+		_ = job.Scheduler.RemoveByTag(job.Tag)
+	}
+
+	bot.Jobs = make([]bot.JobInfo, 0)
+}
+
+// RegisterJobs will go through bot.Jobs and handle the re-registration of them
+func RegisterJobs() {
+	for _, job := range bot.Jobs {
+		// Run job if it doesn't have checking enabled, or if the condition is true
+		if !job.CheckCondition || job.Condition {
+			if rJob, err := job.Scheduler.Tag(job.Tag).Do(job.Fn); err != nil {
+				log.Printf("failed to register job (%s): %v\n", job.Tag, err)
+			} else {
+				log.Printf("registered job: %v\n", rJob)
+			}
+		}
+	}
+}
+
+// RegisterAll will register all bot features, and then load plugins
+func RegisterAll(dir, pluginList string) {
+	bot.Mutex.Lock()
+	defer bot.Mutex.Unlock()
+
+	// This is done to clear the existing plugins that have already been registered, if this is called after the bot
+	// has already been initialized. This allows reloading plugins at runtime.
+	bot.Commands = make([]bot.CommandInfo, 0)
+	bot.Responses = make([]bot.ResponseInfo, 0)
+
+	// We want to do this before registering plugins
+	ClearJobs()
+
+	// This registers the plugins we have downloaded
+	// This does not build new plugins for us, which instead has to be done separately
+	Load(dir, pluginList)
+
+	// This registers the new jobs that plugins have scheduled
+	RegisterJobs()
 }
 
 func parsePluginsList(pluginList string) []string {
