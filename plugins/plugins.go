9cc13db48a07bd484c8eb047cfc8d9e670e23cad
5HT2 <git@frogg.ie>
2022-06-17 19:59:00 -0400

    impr: Add `GuildMemberRemoveEvent` to managed Handler FnTypes

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -207,6 +207,10 @@ func RegisterHandlers() {
 			fn = func(e *gateway.GuildMemberAddEvent) {
 				handler.Fn(e)
 			}
+		case reflect.TypeOf(func(e *gateway.GuildMemberRemoveEvent) {}):
+			fn = func(e *gateway.GuildMemberRemoveEvent) {
+				handler.Fn(e)
+			}
 		default:
 			log.Printf("failed to register handler (%s): type %v not recognized\n", handler.FnName, handler.FnType)
 			continue
