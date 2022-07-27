ced8622c0931c397226e9f3cf01f9285689b8521
5HT2 <git@frogg.ie>
2022-07-27 18:04:57 +0200

    impr: (formatting.go) Add `JoinIntAndStr` and `JoinInt64AndStr`

--- util/formatting.go
+++ util/formatting.go
@@ -28,27 +28,19 @@ func GetUserMention(id int64) string {
 
 // FormattedTime will turn seconds into a pretty time representation
 func FormattedTime(secondsIn int64) string {
-	joinIntAndStr := func(int int64, str string) string {
-		plural := "s"
-		if int == 1 {
-			plural = ""
-		}
-		return fmt.Sprintf("%s %s%s", FormattedNum(int), str, plural)
-	}
-
 	hours := secondsIn / 3600
 	minutes := (secondsIn / 60) - (60 * hours)
 	seconds := secondsIn % 60
 
 	units := make([]string, 0)
 	if hours != 0 {
-		units = append(units, joinIntAndStr(hours, "hour"))
+		units = append(units, JoinInt64AndStr(hours, "hour"))
 	}
 	if minutes != 0 {
-		units = append(units, joinIntAndStr(minutes, "minute"))
+		units = append(units, JoinInt64AndStr(minutes, "minute"))
 	}
 	if seconds != 0 || (hours == 0 && minutes == 0) {
-		units = append(units, joinIntAndStr(seconds, "second"))
+		units = append(units, JoinInt64AndStr(seconds, "second"))
 	}
 
 	return strings.Join(units, ", ")
@@ -57,4 +49,18 @@ func FormattedTime(secondsIn int64) string {
 // FormattedNum will insert commas as necessary in large numbers
 func FormattedNum(num int64) string {
 	return printer.Sprintf("%d", num)
+}
+
+// JoinInt64AndStr will join and add a plural s to the str if int is not 1, for example, "0 hours", "1 hour", "2 hours".
+func JoinInt64AndStr(int int64, str string) string {
+	plural := "s"
+	if int == 1 {
+		plural = ""
+	}
+	return fmt.Sprintf("%s %s%s", FormattedNum(int), str, plural)
+}
+
+// JoinIntAndStr is a wrapper for JoinInt64AndStr
+func JoinIntAndStr(int int, str string) string {
+	return JoinInt64AndStr(int64(int), str)
 }
