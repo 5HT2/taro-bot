3f1caae2f6997d5269a45dfa11c3e85c47a3a5b5
5HT2 <git@frogg.ie>
2022-11-22 23:23:48 -0800

    impr: (http) Add `RequestUrlFn()` and `RequestUrlReq()`

--- util/http.go
+++ util/http.go
@@ -3,7 +3,7 @@ package util
 import (
 	"github.com/5HT2/taro-bot/bot"
 	"github.com/5HT2C/http-bash-requests/httpBashRequests"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"time"
 )
@@ -15,6 +15,22 @@ func RequestUrl(url string, method string) ([]byte, *http.Response, error) {
 		return nil, nil, err
 	}
 
+	return RequestUrlReq(req)
+}
+
+// RequestUrlFn will execute fn on req, then return the bytes of the body of url
+func RequestUrlFn(url string, method string, fn func(req *http.Request)) ([]byte, *http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	fn(req)
+
+	return RequestUrlReq(req)
+}
+
+// RequestUrlReq will return the bytes of the body of request
+func RequestUrlReq(req *http.Request) ([]byte, *http.Response, error) {
 	res, err := bot.HttpClient.Do(req)
 	if err != nil {
 		return nil, nil, err
@@ -24,7 +40,7 @@ func RequestUrl(url string, method string) ([]byte, *http.Response, error) {
 		defer res.Body.Close()
 	}
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, nil, err
 	}
